6e19af5b3783acfea3798b98a387a62c528eeed1
Sejla Jahic <sejla_jahic@hotmail.com>
2019-10-30 13:17:04 +0100

    first

---
+++ src/com/Collections/Collections.java
@@ -0,0 +1,10 @@
+package com.Collections;
+
+public interface Collections {
+	 public boolean add(Object o);
+	 public void clear();
+	 public boolean contains(Object o);
+	 public boolean isEmpty();
+	 public boolean remove(Object o);
+	 public int size();
+}
